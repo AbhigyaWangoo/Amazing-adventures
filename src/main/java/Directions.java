1523e070396b1a78b3e153faa2809bc6bd8348a7
abhigya wangoo <dnathunder@gmail.com>
2020-09-12 10:10:39 -0700

    Created enum values for directions player can move in

---
+++ src/main/java/Directions.java
@@ -0,0 +1,8 @@
+public enum Directions{
+    North, // head north in map
+    South, // head south in map
+    East, // head east in map
+    West, // head west in map
+    Inside, // head inside room/building in map
+    Outside // head outside room/building in map
+}
