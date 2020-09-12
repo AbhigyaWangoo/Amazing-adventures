c6b761515cd8f151427a844f6ec154af081eabb6
abhigya wangoo <dnathunder@gmail.com>
2020-09-12 10:10:17 -0700

    Added classes for object decomposition

---
+++ src/main/java/RoomMap.java
@@ -0,0 +1,30 @@
+import java.util.List;
+
+/**
+ * Class for constructing map of rooms, works similar to a non-binary tree
+ */
+public class RoomMap {
+    private String roomName;
+    private List<RoomMap> directions;
+
+    public RoomMap(String roomName, List<RoomMap> directions) {
+        this.roomName = roomName;
+        this.directions = directions;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public List<RoomMap> getDirections() {
+        return directions;
+    }
+
+    public void setDirections(List<RoomMap> directions) {
+        this.directions = directions;
+    }
+}
---
+++ src/main/java/UISetup.java
@@ -0,0 +1,11 @@
+public class UISetup {
+    private String userInput;
+
+    public String getUserInput() {
+        return userInput;
+    }
+
+    public void setUserInput(String userInput) {
+        this.userInput = userInput;
+    }
+}
