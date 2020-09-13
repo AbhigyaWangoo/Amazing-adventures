d3633d9a51601583da1911449c57d2315cb4f588
abhigya wangoo <dnathunder@gmail.com>
2020-09-13 14:09:42 -0700

    Wrote tests for item pickup

--- src/test/java/student/adventure/AdventureTest.java
+++ src/test/java/student/adventure/AdventureTest.java
@@ -4,15 +4,21 @@ import org.hamcrest.CoreMatchers;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 
 public class AdventureTest {
     GameEngine gameEngine = new GameEngine();
-
+    Player player = new Player();
     @Before
     public void setUp() {
         gameEngine = new GameEngine();
+        player.setCurrentRoom(gameEngine.getRooms().getRooms().get("soccerField"));
+        player.setCurrentRoomName("soccerField");
+        player.setInventory(new ArrayList<>());
+
         gameEngine.deserialize();
     }
 
@@ -45,6 +51,14 @@ public class AdventureTest {
 
     @Test
     public void testItemPickup() {
+        player.pickUpItem("ball", gameEngine.getRooms());
+
+        gameEngine.getRooms().getRooms().get("soccerField").getItems().remove(0);
+        RoomDetail finalRoom = gameEngine.getRooms().getRooms().get("soccerField");
+
+        if(player.getInventory().contains("ball") && !finalRoom.getItems().contains("ball")){
+            assertTrue(true);
+        }
     }
 
     // Boundary Cases
@@ -57,6 +71,14 @@ public class AdventureTest {
         } catch (IllegalArgumentException e){
             assertTrue(true);
         }
+    }
 
+    @Test
+    public void testNonexistantItem() {
+        try{
+            player.pickUpItem("the state of Illinois", gameEngine.getRooms());
+        } catch (RuntimeException e){
+            assertTrue(true);
+        }
     }
 }
