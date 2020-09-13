328b9188c6d2db48c7293b380ed772f1a3c87e97
abhigya wangoo <dnathunder@gmail.com>
2020-09-13 14:10:26 -0700

    new function and member variable for item pickup

--- src/main/java/student/adventure/Player.java
+++ src/main/java/student/adventure/Player.java
@@ -7,6 +7,15 @@ import java.util.List;
 public class Player {
     private List<String> inventory;
     private RoomDetail currentRoom;
+    private String currentRoomName;
+
+    public String getCurrentRoomName() {
+        return currentRoomName;
+    }
+
+    public void setCurrentRoomName(String currentRoomName) {
+        this.currentRoomName = currentRoomName;
+    }
 
     public RoomDetail getCurrentRoom() {
         return currentRoom;
@@ -31,26 +40,33 @@ public class Player {
     public void move(String direction, RoomMap roomMap, RoomDetail currentRoom){
         switch(getFormattedString(direction)){
             case "west":
+                setCurrentRoomName(currentRoom.getDirections().getWestRoom());
                 setCurrentRoom(westRoom(roomMap,currentRoom));
+
                 break;
 
             case "east":
+                setCurrentRoomName(currentRoom.getDirections().getEastRoom());
                 setCurrentRoom(eastRoom(roomMap,currentRoom));
                 break;
 
             case "north":
+                setCurrentRoomName(currentRoom.getDirections().getNorthRoom());
                 setCurrentRoom(northRoom(roomMap,currentRoom));
                 break;
 
             case "south":
+                setCurrentRoomName(currentRoom.getDirections().getSouthRoom());
                 setCurrentRoom(southRoom(roomMap,currentRoom));
                 break;
 
             case "inside":
+                setCurrentRoomName(currentRoom.getDirections().getInsideRoom());
                 setCurrentRoom(insideRoom(roomMap,currentRoom));
                 break;
 
             case "outside":
+                setCurrentRoomName(currentRoom.getDirections().getOutsideRoom());
                 setCurrentRoom(outsideRoom(roomMap,currentRoom));
                 break;
 
@@ -59,27 +75,40 @@ public class Player {
         }
     }
 
-    // helper functions
+    public void pickUpItem(String item, RoomMap roomMap){
+        List<String> items = roomMap.getRooms().get(currentRoomName).getItems();
+        try{
+            int indexOf = items.indexOf(item);
+
+            inventory.add(items.get(indexOf)); // Adds item picked up to inventory
+            items.remove(indexOf); // Removes item from room
+
+            roomMap.getRooms().get(currentRoomName).setItems(items);
+        } catch(Exception e){
+            throw new RuntimeException();
+        }
+    }
+
+    // Helper functions
 
     // following functions return a RoomDetail object of the room in the given direction
     private RoomDetail westRoom(RoomMap roomMap, RoomDetail currentRoom){
-        String westRoom = currentRoom.getDirections().getWest();
-        return roomMap.getRooms().get(westRoom);
+        return roomMap.getRooms().get(currentRoom.getDirections().getWestRoom());
     }
     private RoomDetail eastRoom(RoomMap roomMap, RoomDetail currentRoom){
-        return roomMap.getRooms().get(currentRoom.getDirections().getEast());
+        return roomMap.getRooms().get(currentRoom.getDirections().getEastRoom());
     }
     private RoomDetail northRoom(RoomMap roomMap, RoomDetail currentRoom){
-        return roomMap.getRooms().get(currentRoom.getDirections().getNorth());
+        return roomMap.getRooms().get(currentRoom.getDirections().getNorthRoom());
     }
     private RoomDetail southRoom(RoomMap roomMap, RoomDetail currentRoom){
-        return roomMap.getRooms().get(currentRoom.getDirections().getSouth());
+        return roomMap.getRooms().get(currentRoom.getDirections().getSouthRoom());
     }
     private RoomDetail insideRoom(RoomMap roomMap, RoomDetail currentRoom){
-        return roomMap.getRooms().get(currentRoom.getDirections().getInside());
+        return roomMap.getRooms().get(currentRoom.getDirections().getInsideRoom());
     }
     private RoomDetail outsideRoom(RoomMap roomMap, RoomDetail currentRoom){
-        return roomMap.getRooms().get(currentRoom.getDirections().getOutside());
+        return roomMap.getRooms().get(currentRoom.getDirections().getOutsideRoom());
     }
 
     private String getFormattedString(String s){
