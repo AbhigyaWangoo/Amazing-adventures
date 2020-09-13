fb11dfb5ce58c27a7542da43ada37899b97178fe
abhigya wangoo <dnathunder@gmail.com>
2020-09-13 11:51:12 -0700

    Added GameEngine for deserialization function

---
+++ src/main/java/student/adventure/GameEngine.java
@@ -0,0 +1,51 @@
+package student.adventure;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GameEngine {
+  private Room room = new Room();
+
+  /**
+   * Standard function to deserialize JSON file into room member variable
+   * @return true if successful, false if otherwise
+   */
+  public boolean deserialize() {
+    Gson gson = new Gson();
+    try {
+      String jsonString =
+          readFileAsString(
+              "/Users/abhigyawangoo/IdeaProjects/amazing-adventures-AbhiWangoo/src/main/resources/AdventureMap.json");
+      Type type = new TypeToken<HashMap<String, RoomDetail>>() {}.getType();
+
+      Map<String, RoomDetail> map = gson.fromJson(jsonString, type);
+      room.setRooms(map);
+
+      return true;
+    } catch (Exception e) {
+      e.printStackTrace();
+      return false;
+    }
+  }
+
+  public void play() {}
+
+
+  // helper methods
+  /**
+   * Converts File to String
+   * @param file for reading
+   * @return String value of file
+   * @throws Exception in case of readability error
+   */
+  public static String readFileAsString(String file) throws Exception {
+    return new String(Files.readAllBytes(Paths.get(file)));
+  }
+}
