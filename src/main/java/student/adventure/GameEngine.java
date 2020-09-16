9f2311ee7d3923e08ee4007a424f1f5946d750b6
abhigya wangoo <dnathunder@gmail.com>
2020-09-15 19:27:53 -0700

    Reformatted for javadoc and better object organization

--- src/main/java/student/adventure/GameEngine.java
+++ src/main/java/student/adventure/GameEngine.java
@@ -4,7 +4,6 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -13,7 +12,6 @@ import java.util.*;
 public class GameEngine {
   private RoomMap rooms;
   private Player player;
-  private UIOperations uiOperations;
 
   public GameEngine() {
     setUpGame();
@@ -38,7 +36,6 @@ public class GameEngine {
     rooms = new RoomMap();
     String file =
         "/Users/abhigyawangoo/IdeaProjects/amazing-adventures-AbhiWangoo/src/main/resources/AdventureMap.json";
-
     try {
       String jsonString = readFileAsString(file);
       Type type = new TypeToken<HashMap<String, RoomDetail>>() {}.getType();
@@ -60,20 +57,18 @@ public class GameEngine {
    */
   public void play(InputStream inputStream) {
     try {
-      uiOperations = new UIOperations();
-      uiOperations.welcomeMessage();
+      UIOperations.welcomeMessage();
 
       String userInput = "";
       Scanner scanner = new Scanner(inputStream);
 
-      while (!userInput.toLowerCase().equals(Constants.EXIT)) {
-        uiOperations.examine(player);
+      while (!userInput.toLowerCase().equals(Constant.EXIT)) {
+        UIOperations.examine(player);
         userInput = scanner.nextLine().toLowerCase();
-        try{
+        try {
           userInput = followCommand(userInput);
         } catch (IllegalArgumentException e) {
           System.out.println(e.getMessage());
-          uiOperations.displayErrorUnknownCommand();
         }
       }
     } catch (Exception e) {
@@ -85,7 +80,7 @@ public class GameEngine {
   /** Sets up entire game, loads JSON, assigns player to first room */
   private void setUpGame() {
     deserialize();
-    player = new Player(rooms, player.getStartingRoomName());
+    player = new Player(rooms, "soccerField");
   }
 
   /**
@@ -100,8 +95,8 @@ public class GameEngine {
   }
 
   /**
-   * Follows and executes command from user input provided in play(). Returns empty string if
-   * command isn't recognizable
+   * Follows and executes command from user input provided in play(). Throws
+   * IllegalArgumentException if the user's input isn't in the form (command) (parameter)
    *
    * @param input represents user input from play()
    * @return empty String if theres no problem with command, "Unknown Command" if otherwise
@@ -110,26 +105,35 @@ public class GameEngine {
     String[] commandParameters = input.split(" ");
 
     if (commandParameters.length != 2) {
-      throw new IllegalArgumentException(UIOperations.ERR_UNKNOWN_CMD);
+      throw new IllegalArgumentException(UIOperations.unknownCommand());
     }
 
     String command = commandParameters[0]; // action
     String parameter = commandParameters[1]; // action specification
 
     List<String> commands = new ArrayList<>();
-    commands.add(Constants.GO);
-    commands.add(Constants.DROP);
-    commands.add(Constants.TAKE);
-    commands.add(Constants.START);
-    commands.add(Constants.EXIT);
-    commands.add(Constants.EXAMINE); // potentially needs to be moved
+    commands.add(Constant.GO);
+    commands.add(Constant.DROP);
+    commands.add(Constant.TAKE);
+    commands.add(Constant.START);
+    commands.add(Constant.EXIT);
+    commands.add(Constant.EXAMINE); // potentially needs to be moved
 
     return determineCommand(commands, command, parameter);
   }
 
+  /**
+   * Returns String which corresponds to proper commands object. Then appropriately changes player
+   * based on command. If command doesn't exist, throws IllegalArgumentException
+   *
+   * @param commands includes list of acceptable commands
+   * @param command has command for checking
+   * @param parameter has parameter for specification of direction, item, etc.
+   * @return String exit if user wants to quit, exception otherwise
+   */
   private String determineCommand(List<String> commands, String command, String parameter) {
     if (!commands.contains(command)) {
-      throw new IllegalArgumentException(UIOperations.ERR_UNKNOWN_CMD);
+      throw new IllegalArgumentException(UIOperations.unknownCommand());
     }
 
     if (commands.get(0).compareTo(command) == 0) {
@@ -138,38 +142,57 @@ public class GameEngine {
       playerDropCommand(parameter);
     } else if (commands.get(2).compareTo(command) == 0) {
       playerPickUpCommand(parameter);
-    } else if (commands.get(3).compareTo(Constants.START) == 0) {
+    } else if (commands.get(3).compareTo(Constant.START) == 0) {
       player.playerMoveBackToStart();
-    } else if (command.compareTo(Constants.EXIT) == 0) {
-      return Constants.EXIT;
+    } else if (command.compareTo(Constant.EXIT) == 0) {
+      return Constant.EXIT;
     }
-    throw new IllegalArgumentException(UIOperations.ERR_UNKNOWN_CMD);
+
+    throw new IllegalArgumentException("determine commang exception thrown 2");
   }
 
+  /**
+   * Called when user inputs "take" command. In case of an item which doesn't exist, function throws
+   * an IllegalArgumentException with a message of error on the command line.
+   *
+   * @param parameter specifies the item to take
+   */
   private void playerPickUpCommand(String parameter) {
     if (!player.isValidAvailableItem(parameter)) {
-      uiOperations.displayErrorNoItemAvailable(parameter);
+      throw new IllegalArgumentException(UIOperations.displayErrorNoItemAvailable(parameter));
     } else {
       player.pickUpItem(parameter, rooms);
     }
   }
 
+  /**
+   * Called when user inputs "drop" command. In case item doesn't exist in the inventory, or the
+   * item already exists in the room, function throws an IllegalArgumentException with a message of
+   * error on the command line.
+   *
+   * @param parameter specifies the item to drop
+   */
   private void playerDropCommand(String parameter) {
     if (!player.isValidItemDrop(parameter)) {
-      uiOperations.displayErrorDirectionMessage(parameter);
+      throw new IllegalArgumentException(UIOperations.displayErrorDroppingItem(parameter));
     } else if (player.isValidAvailableItem(parameter)) {
-      uiOperations.displayErrorNoItemAvailable(parameter);
+      throw new IllegalArgumentException(UIOperations.displayErrorItemDuplicate(parameter));
     } else {
       player.dropItem(parameter, rooms);
     }
   }
 
+  /**
+   * Called when user inputs "go" command. In case the direction isn't valid, function throws an
+   * IllegalArgumentException with message of error
+   *
+   * @param parameter specifies the direction to move in
+   */
   private void playerMovementCommand(String parameter) {
     if (!player.isValidDirection(parameter)) {
-      uiOperations.displayErrorDirectionMessage(parameter);
+      throw new IllegalArgumentException(UIOperations.errorDirectionMessage(parameter));
     } else {
       player.move(parameter, rooms, player.getCurrentRoom());
     }
   }
-
 }
--- src/main/java/student/adventure/Player.java
+++ src/main/java/student/adventure/Player.java
@@ -2,191 +2,214 @@ package student.adventure;
 
 import java.util.ArrayList;
 import java.util.List;
-/**
- * Class decides the player's current information
- */
+
+/** Class decides the player's current information */
 public class Player {
-    private List<String> inventory;
-    private RoomDetail currentRoom;
-    private RoomDetail startingRoom;
-    private String currentRoomName;
-    private String startingRoomName;
+  private List<String> inventory;
+  private RoomDetail currentRoom;
+  private RoomDetail startingRoom;
+  private String currentRoomName;
+  private String startingRoomName;
 
   // Constructor sets current room to starting room and sets starting room values
   public Player(RoomMap roomMap, String initialRoomName) {
-        RoomDetail room = roomMap.getRooms().get(initialRoomName);
+    RoomDetail room = roomMap.getRooms().get(initialRoomName);
 
-        setCurrentRoom(room);
-        setStartingRoom(room);
+    setCurrentRoom(room);
+    setStartingRoom(room);
 
-        setCurrentRoomName(initialRoomName);
-        setStartingRoomName(initialRoomName);
+    setCurrentRoomName(initialRoomName);
+    setStartingRoomName(initialRoomName);
 
-        setInventory(new ArrayList<>());
+    setInventory(new ArrayList<>());
+  }
+
+  public RoomDetail getStartingRoom() {
+    return startingRoom;
+  }
+
+  public void setStartingRoom(RoomDetail startingRoom) {
+    this.startingRoom = startingRoom;
+  }
+
+  public String getStartingRoomName() {
+    return startingRoomName;
+  }
+
+  public void setStartingRoomName(String startingRoomName) {
+    this.startingRoomName = startingRoomName;
+  }
+
+  public String getCurrentRoomName() {
+    return currentRoomName;
+  }
+
+  public void setCurrentRoomName(String currentRoomName) {
+    this.currentRoomName = currentRoomName;
+  }
+
+  public RoomDetail getCurrentRoom() {
+    return currentRoom;
+  }
+
+  public void setCurrentRoom(RoomDetail currentRoom) {
+    this.currentRoom = currentRoom;
+  }
+
+  public List<String> getInventory() {
+    return inventory;
+  }
+
+  public void setInventory(List<String> inventory) {
+    this.inventory = inventory;
+  }
+
+  /**
+   * Moves the player into specific direction, doesn't need error handling, all is done in
+   * GameEngine
+   *
+   * @param direction determines direction to move player to
+   * @param roomMap references map the player is in
+   * @param currentRoom contains details about current room player is in
+   */
+  public void move(String direction, RoomMap roomMap, RoomDetail currentRoom) {
+    switch (UIOperations.getFormattedString(direction)) {
+      case Constant.WEST:
+        setCurrentRoomName(currentRoom.getDirections().getWestRoom());
+        setCurrentRoom(westRoom(roomMap));
+        break;
+
+      case Constant.EAST:
+        setCurrentRoomName(currentRoom.getDirections().getEastRoom());
+        setCurrentRoom(eastRoom(roomMap));
+        break;
+
+      case Constant.NORTH:
+        setCurrentRoomName(currentRoom.getDirections().getNorthRoom());
+        setCurrentRoom(northRoom(roomMap));
+        break;
+
+      case Constant.SOUTH:
+        setCurrentRoomName(currentRoom.getDirections().getSouthRoom());
+        setCurrentRoom(southRoom(roomMap));
+        break;
+
+      case Constant.INSIDE:
+        setCurrentRoomName(currentRoom.getDirections().getInsideRoom());
+        setCurrentRoom(insideRoom(roomMap));
+        break;
+
+      case Constant.OUTSIDE:
+        setCurrentRoomName(currentRoom.getDirections().getOutsideRoom());
+        setCurrentRoom(outsideRoom(roomMap));
+        break;
+
+      default:
+        throw new IllegalArgumentException();
+    }
+  }
+
+  /**
+   * Function for picking up items in rooms, error handling addressed in GameEngine
+   *
+   * @param item to pickup
+   * @param roomMap references map the player is in
+   */
+  public void pickUpItem(String item, RoomMap roomMap) {
+    List<String> items = roomMap.getRooms().get(currentRoomName).getItems();
+    int indexOf = items.indexOf(item);
+
+    if (indexOf == -1) {
+      throw new NullPointerException();
     }
 
-    public RoomDetail getStartingRoom() {
-        return startingRoom;
-    }
+    inventory.add(items.get(indexOf)); // Adds item picked up to inventory
+    items.remove(indexOf); // Removes item from room
 
-    public void setStartingRoom(RoomDetail startingRoom) {
-        this.startingRoom = startingRoom;
-    }
+    roomMap.getRooms().get(currentRoomName).setItems(items);
+  }
 
-    public String getStartingRoomName() {
-        return startingRoomName;
-    }
+  /**
+   * Function for dropping items in rooms, throws illegal argument exception if item doesn't exist
+   *
+   * @param item to drop
+   * @param roomMap references map the player is in
+   */
+  public void dropItem(String item, RoomMap roomMap) {
+    List<String> items = roomMap.getRooms().get(currentRoomName).getItems();
 
-    public void setStartingRoomName(String startingRoomName) {
-        this.startingRoomName = startingRoomName;
-    }
+    inventory.remove(item); // Drops item from inventory
+    items.add(item); // Adds item to room
 
-    public String getCurrentRoomName() {
-        return currentRoomName;
-    }
+    roomMap.getRooms().get(currentRoomName).setItems(items);
+  }
 
-    public void setCurrentRoomName(String currentRoomName) {
-        this.currentRoomName = currentRoomName;
-    }
+  /**
+   * Returns whether the direction is valid for the room
+   *
+   * @param direction to check for validation
+   * @return whether the direction is valid or not
+   */
+  public boolean isValidDirection(String direction) {
+    return getCurrentRoom().getDirections().findAvailableDirections().contains(direction);
+  }
 
-    public RoomDetail getCurrentRoom() {
-        return currentRoom;
-    }
+  /**
+   * Returns whether item is available in room
+   *
+   * @param commandParameter to check in room
+   * @return whether is available or not
+   */
+  public boolean isValidAvailableItem(String commandParameter) {
+    return getCurrentRoom().getItems().contains(commandParameter);
+  }
 
-    public void setCurrentRoom(RoomDetail currentRoom) {
-        this.currentRoom = currentRoom;
-    }
+  /**
+   * Returns whether item is available in inventory
+   * @param item to check in inventory
+   * @return whether it exists or not
+   */
+  public boolean isValidItemDrop(String item) {
+    return getInventory().contains(item);
+  }
 
-    public List<String> getInventory() {
-        return inventory;
-    }
+  /**
+   * Moves player back to starting room
+   */
+  public void playerMoveBackToStart() {
+    setCurrentRoom(startingRoom);
+    setCurrentRoomName(startingRoomName);
+  }
 
-    public void setInventory(List<String> inventory) {
-        this.inventory = inventory;
-    }
+  /**
+   * All functions below return RoomDetail object of the specified direction
+   *
+   * @param roomMap for the map of Rooms
+   * @return RoomDetail object of the room in the specified direction
+   */
 
-    /**
-     * Moves the player into specific direction
-     * @param direction determines direction to move player to
-     * @param roomMap references map the player is in
-     * @param currentRoom contains details about current room player is in
-     */
-    public void move(String direction, RoomMap roomMap, RoomDetail currentRoom){
-        switch(getFormattedString(direction)){
-            case Constants.WEST:
-                setCurrentRoomName(currentRoom.getDirections().getWestRoom());
-                setCurrentRoom(westRoom(roomMap,currentRoom));
-                break;
+  // Helper functions
+  private RoomDetail westRoom(RoomMap roomMap) {
+    return roomMap.getRooms().get(currentRoom.getDirections().getWestRoom());
+  }
 
-            case Constants.EAST:
-                setCurrentRoomName(currentRoom.getDirections().getEastRoom());
-                setCurrentRoom(eastRoom(roomMap,currentRoom));
-                break;
+  private RoomDetail eastRoom(RoomMap roomMap) {
+    return roomMap.getRooms().get(currentRoom.getDirections().getEastRoom());
+  }
 
-            case Constants.NORTH:
-                setCurrentRoomName(currentRoom.getDirections().getNorthRoom());
-                setCurrentRoom(northRoom(roomMap,currentRoom));
-                break;
+  private RoomDetail northRoom(RoomMap roomMap) {
+    return roomMap.getRooms().get(currentRoom.getDirections().getNorthRoom());
+  }
 
-            case Constants.SOUTH:
-                setCurrentRoomName(currentRoom.getDirections().getSouthRoom());
-                setCurrentRoom(southRoom(roomMap,currentRoom));
-                break;
+  private RoomDetail southRoom(RoomMap roomMap) {
+    return roomMap.getRooms().get(currentRoom.getDirections().getSouthRoom());
+  }
 
-            case Constants.INSIDE:
-                setCurrentRoomName(currentRoom.getDirections().getInsideRoom());
-                setCurrentRoom(insideRoom(roomMap,currentRoom));
-                break;
+  private RoomDetail insideRoom(RoomMap roomMap) {
+    return roomMap.getRooms().get(currentRoom.getDirections().getInsideRoom());
+  }
 
-            case Constants.OUTSIDE:
-                setCurrentRoomName(currentRoom.getDirections().getOutsideRoom());
-                setCurrentRoom(outsideRoom(roomMap,currentRoom));
-                break;
-
-            default:
-                throw new IllegalArgumentException();
-        }
-    }
-
-    /**
-     * Function for picking up items in rooms, throws runtime exception if item doesn't exist
-     * @param item to pickup
-     * @param roomMap references map the player is in
-     */
-    public void pickUpItem(String item, RoomMap roomMap){
-        List<String> items = roomMap.getRooms().get(currentRoomName).getItems();
-        try{
-            int indexOf = items.indexOf(item);
-            if (indexOf == -1)
-                throw new NullPointerException();
-
-            inventory.add(items.get(indexOf)); // Adds item picked up to inventory
-            items.remove(indexOf); // Removes item from room
-
-            roomMap.getRooms().get(currentRoomName).setItems(items);
-        }catch (NullPointerException e) {
-            System.out.println("Item doesn't exist");
-        } catch(Exception e){
-            throw new RuntimeException();
-        }
-    }
-
-    /**
-     * Function for dropping items in rooms
-     * @param item to drop
-     * @param roomMap references map the player is in
-     */
-    public void dropItem(String item, RoomMap roomMap){
-        List<String> roomItems = roomMap.getRooms().get(currentRoomName).getItems();
-        try{
-            inventory.remove(item); // Drops item from inventory
-            roomItems.add(item); // Adds item to room
-
-            roomMap.getRooms().get(currentRoomName).setItems(roomItems);
-        } catch(Exception e){
-            throw new RuntimeException();
-        }
-    }
-
-    public boolean isValidDirection(String direction) {
-        return getCurrentRoom().getDirections().findAvailableDirections().contains(direction);
-    }
-
-    public boolean isValidAvailableItem(String commandParameter) {
-        return getCurrentRoom().getItems().contains(commandParameter);
-    }
-
-    public boolean isValidItemDrop(String item) {
-        return getInventory().contains(item);
-    }
-
-    public void playerMoveBackToStart() {
-        setCurrentRoom(startingRoom);
-        setCurrentRoomName(startingRoomName);
-    }
-
-    // Helper functions
-    // following functions return a RoomDetail object of the room in the given direction
-    private RoomDetail westRoom(RoomMap roomMap, RoomDetail currentRoom){
-        return roomMap.getRooms().get(currentRoom.getDirections().getWestRoom());
-    }
-    private RoomDetail eastRoom(RoomMap roomMap, RoomDetail currentRoom){
-        return roomMap.getRooms().get(currentRoom.getDirections().getEastRoom());
-    }
-    private RoomDetail northRoom(RoomMap roomMap, RoomDetail currentRoom){
-        return roomMap.getRooms().get(currentRoom.getDirections().getNorthRoom());
-    }
-    private RoomDetail southRoom(RoomMap roomMap, RoomDetail currentRoom){
-        return roomMap.getRooms().get(currentRoom.getDirections().getSouthRoom());
-    }
-    private RoomDetail insideRoom(RoomMap roomMap, RoomDetail currentRoom){
-        return roomMap.getRooms().get(currentRoom.getDirections().getInsideRoom());
-    }
-    private RoomDetail outsideRoom(RoomMap roomMap, RoomDetail currentRoom){
-        return roomMap.getRooms().get(currentRoom.getDirections().getOutsideRoom());
-    }
-
-    private String getFormattedString(String s){
-        return s.replaceAll("\\s","").toLowerCase();
-    }
+  private RoomDetail outsideRoom(RoomMap roomMap) {
+    return roomMap.getRooms().get(currentRoom.getDirections().getOutsideRoom());
+  }
 }
