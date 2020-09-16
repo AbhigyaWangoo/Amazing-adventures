1e81974defe765e1c7d071cc276b91ac935a7ec3
abhigya wangoo <dnathunder@gmail.com>
2020-09-15 21:33:35 -0700

    Added constants

--- src/test/java/student/adventure/AdventureTest.java
+++ src/test/java/student/adventure/AdventureTest.java
@@ -1,77 +1,107 @@
 package student.adventure;
 
-import org.hamcrest.CoreMatchers;
+import com.fasterxml.jackson.core.JsonParser;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import static org.junit.Assert.*;
 
 
 public class AdventureTest {
-    GameEngine gameEngine = new GameEngine();
-    Player player;
+    private GameEngine gameEngine;
+    private Player player;
+    private String args;
+    private ByteArrayInputStream in;
+    private final String SOCCER_FIELD = "soccerField";
+    private final String AMAZON_TOWER = "amazonTower";
+    private final String BALL = "ball";
 
     @Before
     public void setUp() {
-        gameEngine.deserialize();
-        player = new Player(gameEngine.getRooms());
+        gameEngine = new GameEngine();
+        player = new Player(gameEngine.getRooms(), SOCCER_FIELD);
     }
 
+    @After
+    public void closeStreams(){
+        System.out.flush();
+        OutputStream outs = new ByteArrayOutputStream();
+    }
     @Test
     public void testDeserialization() {
         assertTrue(gameEngine.deserialize());
     }
 
+    /*@Test
+    public void isValidJson(){
+        String json = "/Users/abhigyawangoo/IdeaProjects/amazing-adventures-AbhiWangoo/src/main/resources/AdventureMap.json";
+        try{
+            new JsonParser().parse(json);
+        } catch(Exception e){
+            assertFalse(true);
+        }
+    }*/
+
     @Test
     public void testPlayerMove() {
-        RoomDetail currentRoom = gameEngine.getRooms().getRooms().get("soccerField");
-        RoomDetail nextRoom = gameEngine.getRooms().getRooms().get("amazonTower");
+        RoomDetail nextRoom = gameEngine.getRooms().getRooms().get(AMAZON_TOWER);
 
-        player.move("west", gameEngine.getRooms(),currentRoom);
+        player.move("west", gameEngine.getRooms(),player.getStartingRoom());
         assertEquals(player.getCurrentRoom(), nextRoom);
     }
 
     @Test
-    public void testInventoryDisplay(){
-
-    }
-    @Test
     public void testPlayerPath() {
-        String[] args = {"go","east"};
-        gameEngine.play();
-    }
+        GameEngine gameEngine = new GameEngine();
+        InputStream x = System.in;
+        gameEngine.play(x);
 
-    @Test
-    public void testNullPath() {
+        args = "go east";
+        in = new ByteArrayInputStream(args.getBytes());
+
+        Scanner scanner = new Scanner(in);
+
     }
 
     @Test
     public void testItemPickup() {
-        gameEngine.getPlayer().pickUpItem("ball", gameEngine.getRooms());
+        gameEngine.getPlayer().pickUpItem(BALL, gameEngine.getRooms());
 
-        gameEngine.getRooms().getRooms().get("soccerField").getItems().remove(0);
-        RoomDetail finalRoom = gameEngine.getRooms().getRooms().get("soccerField");
+        gameEngine.getRooms().getRooms().get(SOCCER_FIELD).getItems().remove(0);
+        RoomDetail finalRoom = gameEngine.getRooms().getRooms().get(SOCCER_FIELD);
 
-        if(gameEngine.getPlayer().getInventory().contains("ball") && !finalRoom.getItems().contains("ball")){
+
+        if(gameEngine.getPlayer().isValidItemDrop(BALL) && !finalRoom.getItems().contains(BALL)){
             assertTrue(true);
         }
     }
 
     @Test
     public void testItemDrop() {
-        gameEngine.getPlayer().pickUpItem("ball", gameEngine.getRooms());
-        gameEngine.getPlayer().dropItem("ball", gameEngine.getRooms());
+        gameEngine.getPlayer().pickUpItem(BALL, gameEngine.getRooms());
+        gameEngine.getPlayer().dropItem(BALL, gameEngine.getRooms());
         List<String> emptyList = new ArrayList<>();
         assertEquals(emptyList,gameEngine.getPlayer().getInventory());
+    }
+
+    @Test
+    public void testGoToStart(){
+        gameEngine.getPlayer().move("south", gameEngine.getRooms(), player.getCurrentRoom());
+        gameEngine.getPlayer().playerMoveBackToStart();
+
+        assertEquals(gameEngine.getPlayer().getStartingRoom(), gameEngine.getPlayer().getStartingRoom());
     }
 
     // Boundary Cases
     @Test
     public void testNullMovePath() {
-        RoomDetail currentRoom = gameEngine.getRooms().getRooms().get("soccerField");
+        RoomDetail currentRoom = gameEngine.getRooms().getRooms().get(SOCCER_FIELD);
         try{
             gameEngine.getPlayer().move("north", gameEngine.getRooms(), currentRoom);
         } catch (IllegalArgumentException e){
@@ -83,6 +113,16 @@ public class AdventureTest {
     public void testNonexistantItemPickup() {
         try{
             gameEngine.getPlayer().pickUpItem("the state of Illinois", gameEngine.getRooms());
+        } catch (RuntimeException e){
+            assertTrue(true);
+        }
+    }
+
+    @Test
+    public void testDuplicateItemPickup() {
+        try{
+            gameEngine.getPlayer().getInventory().add(BALL);
+            gameEngine.getPlayer().pickUpItem(BALL, gameEngine.getRooms());
         } catch (RuntimeException e){
             assertTrue(true);
         }
